bae45e6ffb218bf7b23b5bed42007d2cef487d31
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2017-01-26 12:25:34 +0100

    Initial Commit

---
+++ TextDetection/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TextDetection
+//
+//  Created by Friedrich HAEUPL on 26.01.17.
+//  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
